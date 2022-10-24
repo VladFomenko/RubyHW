6ec838e93fb263db813ccedc6078c15cecd58327
Vlad Fomenko <cheshirskiy.kot2311@gmail.com>
2022-10-24 22:29:42 +0300

    fix 56.rb

--- HW1/56.rb
+++ HW1/56.rb
@@ -2,11 +2,12 @@
 
 p 'Дан целочисленный массив. Найти максимальное количество подряд идущих максимальных элементов.'
 arr = []
-rand(5..15).times { arr << rand(14..15) }
+rand(5..15).times { arr << rand(11..12) }
 p "(#{arr}.map { |e| e == arr.max ? e : ' ' }.join.squeeze.strip.split.map { |e| e.length / arr.min.to_s.length }.max"
 p arr.map { |e| e == arr.max ? e : ' ' }
      .join
-     .squeeze
+     .squeeze(' ')
      .strip
      .split
-     .map { |e| e.length / arr.min.to_s.length }.max
+     .map { |e| e.length / arr.max.to_s.length }.max
+
