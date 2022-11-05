f2ceebc2b2f763cdbeb0371788ada002744b98c3
Vlad Fomenko <cheshirskiy.kot2311@gmail.com>
2022-11-05 15:37:55 +0200

    update Gem

--- HW3/task1/create_html_page.gemspec
+++ HW3/task1/create_html_page.gemspec
@@ -1,6 +1,6 @@
 Gem::Specification.new do |s|
   s.name        = 'create_html_page'
-  s.version     = '0.0.1'
+  s.version     = '0.0.2'
   s.summary     = 'create html page'
   s.description = 'this method checks, create html page with your content'
   s.authors     = ['Vladyslav Fomenko']
--- HW3/task1/lib/create_html_page.rb
+++ HW3/task1/lib/create_html_page.rb
@@ -7,10 +7,11 @@ class CreateHtmlPage
     page = ['<!DOCTYPE html>', '<html lang="en">', '<head>', '<meta charset="UTF-8">', '<title>New page</title>',
             '</head>', '<body>', '</body>', '</html>']
     file_html = File.new(name, 'w+')
+    pars = CreateHtmlPage.pars(content)
     if bypass_html
-      file_html.write(CreateHtmlPage.add_content(page, content))
+      file_html.write(CreateHtmlPage.add_content(page, pars))
     else
-      file_html.write(CreateHtmlPage.add_content(page, CreateHtmlPage.clear_html(content)))
+      file_html.write(CreateHtmlPage.add_content(page, CreateHtmlPage.clear_html(pars)))
     end
   end
 
@@ -19,6 +20,25 @@ class CreateHtmlPage
   end
 
   def self.clear_html(html)
-    Sanitize.fragment(html)
+    Sanitize.fragment(html, Sanitize::Config::RELAXED)
+  end
+
+  def self.pars(value)
+    if value.is_a?(Hash)
+      value.map { |k, v| "<div>\n<p>#{k} = #{v}</p>\n</div>\n" }.join
+    else
+      value
+    end
   end
 end
+
+c = { health: @health_index,
+      hungry: @hunger_index,
+      thirst: @thirst_index,
+      sleep: @asleep_index,
+      age: @time_index,
+      play: @play_index,
+      love: @love_index,
+      disease: @disease_index }
+
+CreateHtmlPage.create_page(content: c)
