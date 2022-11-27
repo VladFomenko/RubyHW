a367f3d9a04acf2e141dd607f52b9e71278fd2bc
Vlad Fomenko <cheshirskiy.kot2311@gmail.com>
2022-11-27 23:55:28 +0200

    change seed

--- HW6/db/seeds.rb
+++ HW6/db/seeds.rb
@@ -7,7 +7,18 @@
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
 
+Author.all.each { |e| e.destroy  }
+
+user_first = Author.create
+user_second = Author.create
+user_third = Author.create
+@user = [user_first, user_second, user_third]
+
 10.times do
-  test = Article.new(title: FFaker::Book.author, body: FFaker::Book.title)
-  test.save
-end
+  new_user = @user.shuffle
+  new_user[0].articles.create(title: FFaker::Book.author, body: FFaker::Book.title)
+  end
+20.times do
+  new_user = @user.shuffle
+  new_user[0].comments.create(body: FFaker::ColorUA.name, article_id: Article.all.shuffle.first.id)
+  end
