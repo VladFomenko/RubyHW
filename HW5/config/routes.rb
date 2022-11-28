9ee480beb70d907c9672a078e5f216b3660c611b
Vlad Fomenko <cheshirskiy.kot2311@gmail.com>
2022-11-28 21:27:31 +0200

    change routes.rb

--- HW5/config/routes.rb
+++ HW5/config/routes.rb
@@ -1,13 +1,10 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  root 'api/v1/articles#index'
-
   namespace :api do
     namespace :v1 do
       root 'articles#index'
       resources :articles
     end
   end
-
 end
