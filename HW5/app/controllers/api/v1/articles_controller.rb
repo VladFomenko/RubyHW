b8868a1673f0e9bc001f917c3a2be292232a75ce
Vlad Fomenko <cheshirskiy.kot2311@gmail.com>
2022-11-28 21:54:19 +0200

    change articles_controller.rb

--- HW5/app/controllers/api/v1/articles_controller.rb
+++ HW5/app/controllers/api/v1/articles_controller.rb
@@ -1,46 +1,54 @@
 # frozen_string_literal: true
 
-class Api::V1::ArticlesController < ApplicationController
-  before_action :set_article, except: [:index, :create]
+# class ArticlesController
+module Api
+  module V1
+    class ArticlesController < ApplicationController
+      before_action :set_article, except: %i[index create]
 
-  def index
-    @articles = Article.all
-    render json: @articles
-  end
+      def index
+        @articles = Article.all
+        render json: @articles
+      end
 
-  def create
-    @article = Article.create(person_params)
-    if @article.save
-      render json: @article
-    else
-      render json: @article
+      def create
+        @article = Article.new(person_params)
+        if @article.save
+          render_article
+        else
+          render plain: 'Failed to create'
+        end
+      end
+
+      def show
+        render_article
+      end
+
+      def update
+        if @article.update(person_params)
+          render json: @article
+        else
+          render plain: 'Failed to update'
+        end
+      end
+
+      def destroy
+        render plain: 'Deletion successful' if @article.destroy
+      end
+
+      private
+
+      def render_article
+        render json: @article
+      end
+
+      def set_article
+        @article = Article.find(params[:id])
+      end
+
+      def person_params
+        params.require(:article).permit(:title, :body)
+      end
     end
   end
-
-  def show
-    render json: @article
-  end
-
-  def update
-    if @article.update(person_params)
-      render json: @article
-    else
-      render json: @article.errors, status: :unprocessable_entity
-    end
-  end
-
-  def destroy
-    render plain: 'Deletion successful' if @article.destroy
-  end
-
-  private
-
-  def set_article
-    @article = Article.find(params[:id])
-  end
-
-  def person_params
-    params.require(:article).permit(:title, :body)
-  end
-
 end
