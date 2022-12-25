8a54bf60fabf2360f1cf1a4ad632d746707d8c9f
Vlad Fomenko <cheshirskiy.kot2311@gmail.com>
2022-12-25 13:31:59 +0200

    HW9/fix schema

--- HW9/db/migrate/20221127120254_create_comments.rb
+++ HW9/db/migrate/20221127120254_create_comments.rb
@@ -4,7 +4,7 @@
 class CreateComments < ActiveRecord::Migration[7.0]
   def change
     create_table :comments do |t|
-      t.string :body
+      t.string :body, null: false
       t.integer :status, default: 0
       t.references :author, null: false, foreign_key: true
       t.references :article, null: false, foreign_key: true
