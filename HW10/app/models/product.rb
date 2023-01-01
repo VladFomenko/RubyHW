b6328040c255a05eaca53f715f80d624fb4e15b3
Vlad Fomenko <cheshirskiy.kot2311@gmail.com>
2023-01-01 15:44:05 +0200

    add check greater_than:0

--- HW10/app/models/product.rb
+++ HW10/app/models/product.rb
@@ -3,6 +3,6 @@ class Product < ApplicationRecord
 
   validates :name, presence: true, length: { minimum: 2 }
   validates :description, presence: true, length: { minimum: 2 }
-  validates :price, presence: true
+  validates :price, presence: true, numericality: { greater_than: 0 }
   validates :image, presence: true, length: { minimum: 2 }
 end
