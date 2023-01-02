7149ef67dd840d3e81c7b7cbcc7e89138bccac0c
Vlad Fomenko <cheshirskiy.kot2311@gmail.com>
2023-01-02 10:33:03 +0200

    delete HW10

--- HW10/app/models/product.rb
+++
@@ -1,8 +0,0 @@
-class Product < ApplicationRecord
-  belongs_to :category
-
-  validates :name, presence: true, length: { minimum: 2 }
-  validates :description, presence: true, length: { minimum: 2 }
-  validates :price, numericality: { greater_than_or_equal_to: 0 }
-  validates :image, presence: true, length: { minimum: 2 }
-end
--- HW10/db/migrate/20221227214419_add_foreign_key_to_product.rb
+++
@@ -1,5 +0,0 @@
-class AddForeignKeyToProduct < ActiveRecord::Migration[7.0]
-  def change
-    add_reference(:products, :category, foreign_key: true)
-  end
-end
