2d855ee2f251071766b1a0e2daa4d58911d4eb50
Vlad Fomenko <cheshirskiy.kot2311@gmail.com>
2023-01-05 01:51:52 +0200

    HW11/create order new file

---
+++ HW11/app/controllers/orders_controller.rb
@@ -0,0 +1,25 @@
+class OrdersController < ApplicationController
+
+  def index
+    @orders = current_user.orders
+  end
+
+  def create
+    order = current_user.orders.create(cart: current_cart)
+    cookies.delete(:cart_id)
+
+    redirect_to order_path(order), notice: "create order number ##{order.id}"
+  end
+
+  def show
+    @order = current_user.orders.find(params[:id])
+  end
+
+  def paid
+    order = Order.find(params[:id])
+    order.status = 'paid'
+    order.save
+
+    redirect_to order_path(order), notice: "updated ##{order.id}"
+  end
+end
