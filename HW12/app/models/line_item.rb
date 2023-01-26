70f6ce94d5d89bd9f46a33bd4757c5640e313db1
VladFomenko <vlad.fomenko.ua@gmail.com>
2023-01-26 23:14:25 +0200

    HW12/fix new n+1

--- HW12/app/models/line_item.rb
+++ HW12/app/models/line_item.rb
@@ -3,4 +3,7 @@ class LineItem < ApplicationRecord
   belongs_to :cart, optional: true
 
   validates :quantity, numericality: { greater_than_or_equal_to: 1 }
+
+
+  scope :all_product_with_cart, ->(cart_id) { includes([:product]).where('cart_id = ?', cart_id) }
 end
