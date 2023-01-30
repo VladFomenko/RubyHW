6fae4754acb9c626a41b69db353195b6a2351afa
VladFomenko <vlad.fomenko.ua@gmail.com>
2023-01-30 14:45:08 +0200

    HW14/show sidekiq only authorized admin_user

--- HW14/config/routes.rb
+++ HW14/config/routes.rb
@@ -1,28 +1,28 @@
+require 'sidekiq/web'
+
 Rails.application.routes.draw do
   # for show emails
-  mount LetterOpenerWeb::Engine, at: "/letter_opener" if Rails.env.development?
-
-  require 'sidekiq/web'
-  mount Sidekiq::Web => '/sidekiq'
-
-
+  mount LetterOpenerWeb::Engine, at: '/letter_opener' if Rails.env.development?
   # get 'carts/show'
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
+  authenticate :admin_user do
+    mount Sidekiq::Web => '/admin/sidekiq'
+  end
+
   devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  # Defines the root path route ("/")
-  root "products#index"
+  root 'products#index'
   get '/cart', to: 'carts#show', as: :cart
   resources :products, only: %i[index show]
 
   resources :categories
 
-  post 'line_items/:id/add', to: "line_items#add_quantity", as: "line_item_add"
-  post 'line_items/:id/reduce', to: "line_items#reduce_quantity", as: "line_item_reduce"
+  post 'line_items/:id/add', to: 'line_items#add_quantity', as: 'line_item_add'
+  post 'line_items/:id/reduce', to: 'line_items#reduce_quantity', as: 'line_item_reduce'
   resources :line_items
 
-  get 'order/:id/paid', to: "orders#paid", as: "paid"
+  get 'order/:id/paid', to: 'orders#paid', as: 'paid'
   resources :orders, only: %i[index create show]
 end
